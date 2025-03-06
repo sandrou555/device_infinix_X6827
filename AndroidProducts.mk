87969498917eecb4408179a12e7bee428942fa82
TegarXLu <136581622+sandrou555@users.noreply.github.com>
2025-03-06 14:57:41 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_X6831.mk
+    $(LOCAL_DIR)/twrp_X6827.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_X6831-user \
-    twrp_X6831-userdebug \
-    twrp_X6831-eng
+    twrp_X6827-user \
+    twrp_X6827-userdebug \
+    twrp_X6827-eng
